41f2fd27ffef368b8312602f3d3bb9971ab640aa
Venedykt <chewbaccayeti@gmail.com>
2023-07-25 18:54:32 +0300

    prep-work

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        Это CV Вени. Всем привет, рад вас видеть!
+      </div>
+    </>
+  );
+};
+
+export default App;
